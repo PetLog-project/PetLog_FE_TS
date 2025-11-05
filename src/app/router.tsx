13663165a7d293b4ad8478a53a0d52f235995255
seoyerin <seyerin12@naver.com>
2025-11-05 23:32:37 +0900

    feat: 그룹 만들기, 그룹 참여하기 선택 화면 구현

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,7 +1,12 @@
+import { GroupEtry } from "@/features/groupEntry/ui/groupEntry";
 import { MakeGroup } from "@/features/makeGroup/ui/makeGroup/makeGroup";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
+  {
+    path: "/groupentry",
+    element: <GroupEtry />,
+  },
   {
     path: "/makegroup",
     element: <MakeGroup />,
