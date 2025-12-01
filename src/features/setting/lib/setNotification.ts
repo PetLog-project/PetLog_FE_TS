9059b4bec5586c74dac8906d213a82c19e600963
seoyerin <seyerin12@naver.com>
2025-12-02 01:20:34 +0900

    refactor: 세팅 api 연결

--- src/features/setting/lib/setNotification.ts
+++ src/features/setting/lib/setNotification.ts
@@ -1,12 +1,14 @@
 export async function setNotification(
   toggleResult: boolean,
   openModal: (message: string) => void,
+  acc: string,
 ) {
   try {
-    const response = await fetch("", {
+    const response = await fetch("https://dev.petlog.site/api/notification", {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${acc}`,
       },
       body: JSON.stringify({
         isNotificationEnabled: toggleResult,
